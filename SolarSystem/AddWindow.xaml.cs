fb99e0474374f0a9f8cb55f5beae24362e009469
Your Name <you@example.com>
2022-12-24 19:41:45 +0300

    add files

---
+++ SolarSystem/AddWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using SolarSystem.BusinessLogic.Implementation;
+using SolarSystem.Model.DatabaseModels;
+using System;
+using System.Windows;
+
+namespace SolarSystem
+{
+    public partial class AddWindow : Window
+    {
+        private PlanetService _planetService;
+
+        public AddWindow()
+        {
+            InitializeComponent();
+            _planetService = new PlanetService();
+        }
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(NameTb.Text) || !String.IsNullOrEmpty(MassTb.Text) || !String.IsNullOrEmpty(DiametrTb.Text))
+                {
+                    _planetService.Create(new Planet { Name = NameTb.Text, Mass = double.Parse(MassTb.Text), Diameter = double.Parse(DiametrTb.Text) });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ SolarSystem/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SolarSystem.BusinessLogic.Implementation;
+using SolarSystem.Model.DatabaseModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SolarSystem
+{
+    public partial class MainWindow : Window
+    {
+        PlanetService _planetService;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _planetService = new PlanetService();
+            planetsGrid.ItemsSource = _planetService.Get();
+        }
+        private void UpdateGrid()
+        {
+            planetsGrid.ItemsSource = null;
+            planetsGrid.ItemsSource = _planetService.Get();
+        }
+   
+  
+        private void NewWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow win = new AddWindow();
+            win.Show();
+        }
+       
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (planetsGrid.SelectedItems.Count > 0 && MessageBox.Show("Confirm removal", "Removal", MessageBoxButton.YesNo)==MessageBoxResult.Yes )
+            {
+                for (int i = 0; i < planetsGrid.SelectedItems.Count; i++)
+                {
+                    Planet planet = planetsGrid.SelectedItems[i] as Planet;
+                    if (planet != null)
+                    {
+                        _planetService.Delete(planet);
+                    }
+                }
+            }
+            UpdateGrid();  
+        }
+        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            _planetService.Update(e.Row.Item as Planet);
+            UpdateGrid();
+        }
+
+        private void planetsGrid_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
+    }
+}
